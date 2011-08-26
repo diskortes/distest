e561ca9ccaa6db9ff7fcde8c5771aaf0c7ce5954
Igor Dizjichenko <diskortes@gmail.com>
2011-08-26 13:47:18 +0300

    Created view show_mydata and editing settings.py, urls.py

--- distest/main/views.py
+++ distest/main/views.py
@@ -1,1 +1,6 @@
-# Create your views here.
+from django.shortcuts import render_to_response
+from distest.main.models import MyData
+
+def show_mydata(request):
+    my_data = MyData.objects.get(id=1)
+    return render_to_response('mydata.html',{'data':my_data})
