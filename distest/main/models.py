94922f4133012b589ffba4f7921c27d71d110998
Igor Dizjichenko <diskortes@gmail.com>
2011-08-26 13:12:31 +0300

    Created model MyData

--- distest/main/models.py
+++ distest/main/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+class MyData(models.Model):
+    name = models.CharField(max_length=20)
+    last_name = models.CharField(max_length=20)
+    address = models.CharField(max_length=50)
+    bio = models.TextField()
+    email= models.CharField(max_length=20)
